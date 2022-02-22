fb870b78102e3cdc1b4447fd6d7881b4eff51d36
SmithWebDev <smithwebdd@gmail.com>
2022-02-22 14:02:18 -0500

    updated lajan api calls for monthly/weekly timeseries data for model

--- lajan_api_calls.rb
+++ lajan_api_calls.rb
@@ -29,28 +29,31 @@
 # TIMESERIES DATA
 # =================================================================
 client.stock(symbol: ).timeseries.output["Time Series (Daily)"][#{each date}].keys  #=> Daily Time Series information (premium req for adjusted data)
-["1. open", "2. high", "3. low", "4. close", "5. volume"]
+["1. open", "2. high", "3. low", "4. close", "5. volume"],
+
 client.stock(symbol: ).timeseries(outputsize: "full", adjusted: true, type: "monthly").output["Monthly Adjusted Time Series"][#{each date}].keys  #=> Monthly Time Series Information (premium not required)
-["1. open", "2. high", "3. low", "4. close", "5. adjusted close", "6. volume", "7. dividend amount"]
+["1. open", "2. high", "3. low", "4. close", "5. adjusted close", "6. volume", "7. dividend amount"],
+client.stock(symbol: ).timeseries(outputsize: "full", adjusted: true, type: "weekly").output["Weekly Adjusted Time Series"][#{each date}].keys  #=> Monthly Time Series Information (premium not required)
+["1. open", "2. high", "3. low", "4. close", "5. adjusted close", "6. volume", "7. dividend amount"],
 
 # FUNDAMENTAL DATA
 # =================================================================
 client.stock(symbol: ).fundamental_data.overview.keys
-["Symbol", "AssetType", "Name", "Description", "CIK", "Exchange", "Currency", "Country", "Sector", "Industry", "Address", "FiscalYearEnd", "LatestQuarter", "MarketCapitalization", "EBITDA", "PERatio", "PEGRatio", "BookValue", "DividendPerShare", "DividendYield", "EPS", "RevenuePerShareTTM", "ProfitMargin", "OperatingMarginTTM", "ReturnOnAssetsTTM", "ReturnOnEquityTTM", "RevenueTTM", "GrossProfitTTM", "DilutedEPSTTM", "QuarterlyEarningsGrowthYOY", "QuarterlyRevenueGrowthYOY", "AnalystTargetPrice", "TrailingPE", "ForwardPE", "PriceToSalesRatioTTM", "PriceToBookRatio", "EVToRevenue", "EVToEBITDA", "Beta", "52WeekHigh", "52WeekLow", "50DayMovingAverage", "200DayMovingAverage", "SharesOutstanding", "DividendDate", "ExDividendDate"]
+["Symbol", "AssetType", "Name", "Description", "CIK", "Exchange", "Currency", "Country", "Sector", "Industry", "Address", "FiscalYearEnd", "LatestQuarter", "MarketCapitalization", "EBITDA", "PERatio", "PEGRatio", "BookValue", "DividendPerShare", "DividendYield", "EPS", "RevenuePerShareTTM", "ProfitMargin", "OperatingMarginTTM", "ReturnOnAssetsTTM", "ReturnOnEquityTTM", "RevenueTTM", "GrossProfitTTM", "DilutedEPSTTM", "QuarterlyEarningsGrowthYOY", "QuarterlyRevenueGrowthYOY", "AnalystTargetPrice", "TrailingPE", "ForwardPE", "PriceToSalesRatioTTM", "PriceToBookRatio", "EVToRevenue", "EVToEBITDA", "Beta", "52WeekHigh", "52WeekLow", "50DayMovingAverage", "200DayMovingAverage", "SharesOutstanding", "DividendDate", "ExDividendDate"],
 
 client.stock(symbol: ).fundamental_data.balance_sheets["annualReports" or "quarterlyReports"][0].keys
-["fiscalDateEnding", "reportedCurrency", "totalAssets", "totalCurrentAssets", "cashAndCashEquivalentsAtCarryingValue", "cashAndShortTermInvestments", "inventory", "currentNetReceivables", "totalNonCurrentAssets", "propertyPlantEquipment", "accumulatedDepreciationAmortizationPPE", "intangibleAssets", "intangibleAssetsExcludingGoodwill", "goodwill", "investments", "longTermInvestments", "shortTermInvestments", "otherCurrentAssets", "otherNonCurrrentAssets", "totalLiabilities", "totalCurrentLiabilities", "currentAccountsPayable", "deferredRevenue", "currentDebt", "shortTermDebt", "totalNonCurrentLiabilities", "capitalLeaseObligations", "longTermDebt", "currentLongTermDebt", "longTermDebtNoncurrent", "shortLongTermDebtTotal", "otherCurrentLiabilities", "otherNonCurrentLiabilities", "totalShareholderEquity", "treasuryStock", "retainedEarnings", "commonStock", "commonStockSharesOutstanding"]
+["fiscalDateEnding", "reportedCurrency", "totalAssets", "totalCurrentAssets", "cashAndCashEquivalentsAtCarryingValue", "cashAndShortTermInvestments", "inventory", "currentNetReceivables", "totalNonCurrentAssets", "propertyPlantEquipment", "accumulatedDepreciationAmortizationPPE", "intangibleAssets", "intangibleAssetsExcludingGoodwill", "goodwill", "investments", "longTermInvestments", "shortTermInvestments", "otherCurrentAssets", "otherNonCurrrentAssets", "totalLiabilities", "totalCurrentLiabilities", "currentAccountsPayable", "deferredRevenue", "currentDebt", "shortTermDebt", "totalNonCurrentLiabilities", "capitalLeaseObligations", "longTermDebt", "currentLongTermDebt", "longTermDebtNoncurrent", "shortLongTermDebtTotal", "otherCurrentLiabilities", "otherNonCurrentLiabilities", "totalShareholderEquity", "treasuryStock", "retainedEarnings", "commonStock", "commonStockSharesOutstanding"],
 
 client.stock(symbol: ).fundamental_data.income_statements["annualReports" or "quarterlyReports"][0].keys
-["fiscalDateEnding", "reportedCurrency", "grossProfit", "totalRevenue", "costOfRevenue", "costofGoodsAndServicesSold", "operatingIncome", "sellingGeneralAndAdministrative", "researchAndDevelopment", "operatingExpenses", "investmentIncomeNet", "netInterestIncome", "interestIncome", "interestExpense", "nonInterestIncome", "otherNonOperatingIncome", "depreciation", "depreciationAndAmortization", "incomeBeforeTax", "incomeTaxExpense", "interestAndDebtExpense", "netIncomeFromContinuingOperations", "comprehensiveIncomeNetOfTax", "ebit", "ebitda", "netIncome"]
+["fiscalDateEnding", "reportedCurrency", "grossProfit", "totalRevenue", "costOfRevenue", "costofGoodsAndServicesSold", "operatingIncome", "sellingGeneralAndAdministrative", "researchAndDevelopment", "operatingExpenses", "investmentIncomeNet", "netInterestIncome", "interestIncome", "interestExpense", "nonInterestIncome", "otherNonOperatingIncome", "depreciation", "depreciationAndAmortization", "incomeBeforeTax", "incomeTaxExpense", "interestAndDebtExpense", "netIncomeFromContinuingOperations", "comprehensiveIncomeNetOfTax", "ebit", "ebitda", "netIncome"],
 
 client.stock(symbol: ).fundamental_data.cash_flow_statements["annualReports" or "quarterlyReports"][0].keys
-["fiscalDateEnding", "reportedCurrency", "operatingCashflow", "paymentsForOperatingActivities", "proceedsFromOperatingActivities", "changeInOperatingLiabilities", "changeInOperatingAssets", "depreciationDepletionAndAmortization", "capitalExpenditures", "changeInReceivables", "changeInInventory", "profitLoss", "cashflowFromInvestment", "cashflowFromFinancing", "proceedsFromRepaymentsOfShortTermDebt", "paymentsForRepurchaseOfCommonStock", "paymentsForRepurchaseOfEquity", "paymentsForRepurchaseOfPreferredStock", "dividendPayout", "dividendPayoutCommonStock", "dividendPayoutPreferredStock", "proceedsFromIssuanceOfCommonStock", "proceedsFromIssuanceOfLongTermDebtAndCapitalSecuritiesNet", "proceedsFromIssuanceOfPreferredStock", "proceedsFromRepurchaseOfEquity", "proceedsFromSaleOfTreasuryStock", "changeInCashAndCashEquivalents", "changeInExchangeRate", "netIncome"]
+["fiscalDateEnding", "reportedCurrency", "operatingCashflow", "paymentsForOperatingActivities", "proceedsFromOperatingActivities", "changeInOperatingLiabilities", "changeInOperatingAssets", "depreciationDepletionAndAmortization", "capitalExpenditures", "changeInReceivables", "changeInInventory", "profitLoss", "cashflowFromInvestment", "cashflowFromFinancing", "proceedsFromRepaymentsOfShortTermDebt", "paymentsForRepurchaseOfCommonStock", "paymentsForRepurchaseOfEquity", "paymentsForRepurchaseOfPreferredStock", "dividendPayout", "dividendPayoutCommonStock", "dividendPayoutPreferredStock", "proceedsFromIssuanceOfCommonStock", "proceedsFromIssuanceOfLongTermDebtAndCapitalSecuritiesNet", "proceedsFromIssuanceOfPreferredStock", "proceedsFromRepurchaseOfEquity", "proceedsFromSaleOfTreasuryStock", "changeInCashAndCashEquivalents", "changeInExchangeRate", "netIncome"],
 
 # SECTOR DATA
 # =================================================================
 client.sector.output.keys
-["Meta Data", "Rank A: Real-Time Performance", "Rank B: 1 Day Performance", "Rank C: 5 Day Performance", "Rank D: 1 Month Performance", "Rank E: 3 Month Performance", "Rank F: Year-to-Date (YTD) Performance", "Rank G: 1 Year Performance", "Rank H: 3 Year Performance", "Rank I: 5 Year Performance", "Rank J: 10 Year Performance"]
+["Meta Data", "Rank A: Real-Time Performance", "Rank B: 1 Day Performance", "Rank C: 5 Day Performance", "Rank D: 1 Month Performance", "Rank E: 3 Month Performance", "Rank F: Year-to-Date (YTD) Performance", "Rank G: 1 Year Performance", "Rank H: 3 Year Performance", "Rank I: 5 Year Performance", "Rank J: 10 Year Performance"],
 
 # Investing Strategy
 # =================================================================
@@ -62,5 +65,31 @@ client.sector.output.keys
 
 # Dividend Friendly Industries = REITs, MLPs, Tobacco, Telecommunications, Utilities
 
+# Test Companies for project
+%w[AAPL O IRM ARR GROW GAIN LAND]
+
 # Monthly Paying Common Stocks (Does not include preferred not ETF/Exchange Traded Notes)
-%w[ADR, AGNC, ARR, AVAL, BBD, BRMK, CRT, DRETF, DREUF, EFC, EIFZF, EPR, GAIN, GLAD, GOOD, GROW, GWRS, HRZN, IPPLF, ITUB, LAND, LTC, MAIN, MTR, O, ORC, OXSQ, PBA, PBT, PFLT, PPRQF, PRT, PSEC, PVL, SBR, SCM, SJR, SJT, SLG, STAG, SUNS, SUUIF, TRSWF, WSR, EIC]
+%w[ADR, AGNC, ARR, AVAL, BBD, BRMK, CRT, DRETF, DREUF, EFC, EIFZF, EPR, GAIN, GLAD, GOOD, GROW, GWRS, HRZN, IPPLF, ITUB, LAND, LTC, MAIN, MTR, O, ORC, OXSQ, PBA, PBT, PFLT, PPRQF, PRT, PSEC, PVL, SBR, SCM, SJR, SJT, SLG, STAG, SUNS, SUUIF, TRSWF, WSR, EIC],
+
+# Fetch_stock_info
+
+client.stock(symbol: symbol).timeseries(outputsize: "full", adjusted: true, type: "weekly").output["Weekly Adjusted Time Series"].each do |week|
+  weekly_date: week
+  open: week["1. open"],
+  high: week["2. high"],
+  low: week["3. low"],
+  close: week["4. close"],
+  adjusted_close: week["5. adjusted close"],
+  volume: week["6. volume"]
+end
+
+
+client.stock(symbol: symbol).timeseries(outputsize: "full", adjusted: true, type: "monthly").output["Monthly Adjusted Time Series"].each do |month|
+  monthly_date: month
+  open: month["1. open"],
+  high: month["2. high"],
+  low: month["3. low"],
+  close: month["4. close"],
+  adjusted_close: month["5. adjusted close"],
+  volume: month["6. volume"]
+end
--- lib/tasks/fetch_api_calls.rake
+++ lib/tasks/fetch_api_calls.rake
@@ -1,11 +1,11 @@
 desc 'Pull information from the AlphaVantage and IEX Cloud Stock Api'
 
 task fetch_stock_info: :environment do
-  company = Alphavantage::Client.new key: ENV['ALPHA_VANTAGE_KEY']
-  stock = StockQuote::Stock.new(api_key: ENV['STOCK_KEY'])
+  company = Alphavantage::Client.new key: Rails.application.credentials.dig(:alphavantage, :key)
+  stock = StockQuote::Stock.new(api_key: Rails.application.credentials.dig(:stockquote, :key))
 
   list_of_companies = %w[AAPL O IRM ARR GROW GAIN LAND]
 
-  list_of_companies.each do
+  list_of_companies.each do |company|
   end
 end
