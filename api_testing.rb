b66827c7a150217c0bd40f96753a81b92fca07c5
Darrick Smith <smithwebdd@gmail.com>
2022-02-26 18:23:54 -0500

    created file to better compare api data for model creation

---
+++ api_testing.rb
@@ -0,0 +1,18 @@
+StockQuote::Stock.new(api_key: Rails.application.credentials.dig(:stockquote, :key))
+Alphavantage::Client.new key: Rails.application.credentials.dig(:alphavantage, :key)
+
+def company(symbol)
+  StockQuote::Stock.company(symbol)
+end
+
+def quote(symbol)
+  StockQuote::Stock.quote(symbol)
+end
+
+def dividend_history(symbol)
+  StockQuote::Stock.batch('dividends', symbol, '5y')
+end
+
+def dividend_next(symbol)
+  StockQuote::Stock.batch('dividends', symbol, 'next')
+end
