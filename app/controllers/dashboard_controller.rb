66b9ab71c1e99ca11e811c9db71a205b82ffee31
SmithWebDev <smithwebdd@gmail.com>
2022-04-01 02:14:43 -0400

    added account instance method to dashboard controller, working on dashboard view to better visualize information

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,13 @@
 class DashboardController < ApplicationController
+  # before_action :authenticate_user!
+
   def index
     @account = Account.find_by(params[:id])
     @company = Company.all
   end
 
   def trash
+    @account = Account.all
     @company = Company.all
     @accountholding = AccountHolding.all
   end
